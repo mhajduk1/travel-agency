b4209f2b3cab37348d7312aaaf5890d0e2f19492
Michał Hajduk <michal.hajduk@e3.pl>
2020-07-17 10:20:48 +0200

    added api with order options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,34 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col, Grid} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
-import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-class OrderForm extends React.Component {
-  render() {
-    const {tripCost, options, setOrderOption} = this.props;
-    return (
-      <Grid>
-        <Row>
-          {pricing.map(option => (
-            <Col md={4} key={option.id} >
-              <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
-            </Col>
-          ))}
-          <Col xs={12}>
-            <OrderSummary tripCost={tripCost} options={options}/>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
+
+import {Row, Col, Grid} from 'react-flexbox-grid';
+import settings from '../../../data/settings.js';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+import Button from '../../common/Button/Button';
+
+
+const sendOrder = (options, tripCost, tripId, tripName) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  console.log('name', name);
+
+  const payload = {
+    ...options,
+    totalCost,
+    name,
+    tripId,
+    tripName,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+
+      const { name, contact } = options;
+
+      if (name == '') {
+        window.alert('Fill your name');
+      }
+      else if (contact == '') {
+        window.alert('Fill your contact');
+      }
+    });
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, tripId, tripName}) => (
+  <Grid>
+    <Row>
+      {pricing.map(option => (
+        <Col md={4} key={option.id} >
+          <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
+        </Col>
+      ))}
+      <Col xs={12}>
+        <OrderSummary tripCost={tripCost} options={options}/>
+        <Button onClick={() => sendOrder(options, tripCost, tripId, tripName)}>Order now!</Button>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 OrderForm.propTypes = {
-  options: PropTypes.object,
   tripCost: PropTypes.string,
+  options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
 };
 
 export default OrderForm;
